b5cb5880a197cf6de5b38e1576fd84c1d2041f74
Hashim256 <111392305+Hashim256@users.noreply.github.com>
2022-08-23 06:38:13 -0700

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,6 @@
+my_name = "Hashim"
+age = 24
+weight = 60.5
+
+#My name is Hashim, I am 24 years old, I weigh 60.5 kgs
+print("My name is " + my_name + ", I am " + str(age) + " years old "+ ", I weigh " + str(weight) + "kgs")
